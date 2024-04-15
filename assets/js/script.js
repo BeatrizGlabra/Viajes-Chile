e46ba1ad2036b7a17c79184e5a219c940d7ffabd
Beatriz Guerra <mgbeatrizlabra@gmail.com>
2024-04-15 04:14:42 -0400

    quinta version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bstoggle="tooltip"]'))
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)})
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
